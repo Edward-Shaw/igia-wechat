1b67022699dd9ac9dc7ba63520abbe7d77d093b2
Edward-Shaw <yxiao@aliyun.com>
2016-08-14 15:12:54 +0800

    add utils package.

---
+++ src/main/java/com/cloume/shaw/igia/utils/Const.java
@@ -0,0 +1,14 @@
+package com.cloume.shaw.igia.utils;
+
+public class Const {
+	
+	//用户类型
+	@Deprecated public static final int TYPE_UNDEFINE = 0;
+	@Deprecated public static final int TYPE_TEACHER = 1;
+	@Deprecated public static final int TYPE_PARENT = 2;
+	@Deprecated public static final int TYPE_STUDENT = 3;
+	
+	//微信管理后台的用户分组
+	public static final long GROUP_TEACHER = 100;
+	public static final long GROUP_PARENT = 101;
+}
