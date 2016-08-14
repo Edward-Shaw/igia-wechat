308b401a0ac7ee2e2dcc188b2beedaf44d9cd622
Edward-Shaw <yxiao@aliyun.com>
2016-08-14 15:04:26 +0800

    add dependency to chanjar.

---
+++ src/main/java/com/cloume/shaw/igia/wx/resoure/OAuth2AccessToken.java
@@ -0,0 +1,19 @@
+package com.cloume.shaw.igia.wx.resoure;
+
+import org.springframework.data.annotation.Id;
+
+import com.cloume.shaw.igia.utils.Cloner;
+
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
+
+public class OAuth2AccessToken extends WxMpOAuth2AccessToken {
+
+	private static final long serialVersionUID = 3192678856446142417L;
+
+	@Id
+	private String openId;
+	
+	public OAuth2AccessToken(WxMpOAuth2AccessToken wxObject) {
+		new Cloner<WxMpOAuth2AccessToken>(this).clone(wxObject);
+	}
+}
